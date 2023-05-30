48204b761e90556342ebf96e4ddb580895a5b2fa
Shiroyasha1997 <valenzueladiego1997@gmail.com>
2023-05-30 00:22:48 -0400

    Control 2

--- js/script.js
+++ js/script.js
@@ -33,109 +33,6 @@ $(document).ready(function () {
   xhttp.send();
 });
 
-//MANTENEDOR DE PRODUCTOS//
-$(document).ready(function() {
-  // Detectar el evento submit del formulario
-  $("#prod-form").submit(function(event) {
-    // Validar el campo ID
-    if ($("#ID").val() === "") {
-      $("#ID").addClass("is-invalid");
-      event.preventDefault();
-    } else {
-      $("#ID").removeClass("is-invalid");
-    }
-
-    // Validar el campo Nombre
-    if ($("#Nombre").val() === "") {
-      $("#Nombre").addClass("is-invalid");
-      event.preventDefault();
-    } else {
-      $("#Nombre").removeClass("is-invalid");
-    }
-
-    // Validar el campo Descripción
-    if ($("#Descripcion").val() === "") {
-      $("#Descripcion").addClass("is-invalid");
-      event.preventDefault();
-    } else {
-      $("#Descripcion").removeClass("is-invalid");
-    }
-
-    // Validar el campo Precio
-    if ($("#Precio").val() === "") {
-      $("#Precio").addClass("is-invalid");
-      event.preventDefault();
-    } else {
-      $("#Precio").removeClass("is-invalid");
-    }
-
-    // Validar el campo Descuento Suscriptor
-    if ($("#Descuento Suscriptor").val() === "") {
-      $("#Descuento Suscriptor").addClass("is-invalid");
-      event.preventDefault();
-    } else {
-      $("#Descuento Suscriptor").removeClass("is-invalid");
-    }
-
-    // Validar el campo Descuento Oferta
-    if ($("#Descuento Oferta").val() === "") {
-      $("#Descuento Oferta").addClass("is-invalid");
-      event.preventDefault();
-    } else {
-      $("#Descuento Oferta").removeClass("is-invalid");
-    }
-  });
-});
-
-//LOGIN//
-$(document).ready(function() {
-  $('#login-form').submit(function(event) {
-
-    // Evita el envío del formulario
-    event.preventDefault();
-
-    // Obtener los valores de los campos de entrada
-    var email = $('#email').val().trim();
-    var password = $('#password').val().trim();
-
-    // Validar los campos de entrada
-    if (email.length == 0 || password.length == 0) {
-      alert('Por favor, complete todos los campos');
-      return;
-    }
-
-    // Validar el formato del correo electrónico
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      alert('Por favor, introduzca una dirección de correo electrónico válida');
-      return;
-    }
-
-    // Validar la longitud de la contraseña
-    if (password.length < 6) {
-      alert('La contraseña debe tener al menos 6 caracteres');
-      return;
-    }
-
-    // Deshabilitar el botón de inicio de sesión para evitar envíos duplicados
-    $('#btnInciarSesion').prop('disabled', true);
-
-    // Enviar formulario si los campos son válidos
-    $(this).unbind('submit').submit();
-
-    // Redirigir a la página de inicio de sesión si los campos son válidos
-    window.location.href = "index.html";
-
-  });
-
-  // Resaltar el campo de entrada actual al hacer clic en él
-  $('input').focus(function() {
-    $(this).addClass('border-primary');
-  }).blur(function() {
-    $(this).removeClass('border-primary');
-  });
-});
-
 //FOOTER//
 $(document).ready(function () {
   var xhttp = new XMLHttpRequest();
@@ -148,74 +45,386 @@ $(document).ready(function () {
   xhttp.send();
 });
 
-//BODEGA//
-$(document).ready(function() {
-  // Agregar evento de submit al formulario
-  $('#bodega-form').submit(function(event) {
-    // Detener el envío del formulario para poder realizar la validación
-    event.preventDefault();
-    
-    // Obtener los valores de los campos del formulario
-    var categoria = $('#Categoria').val();
-    var producto = $('#Producto').val();
-    var cantidad = $('#Cantidad').val();
-
-    // Validar que la cantidad sea mayor que cero
-    if (cantidad <= 0) {
-      alert('La cantidad debe ser mayor que cero');
-      return;
-    }
-
-    // Si todo está bien, enviar el formulario
-    this.submit();
-  });
-});
-
-//VALIDACION DE REGISTRO//
+//MANTENEDOR DE PRODUCTOS//
 $(document).ready(function () {
-  // Agregar método de validación para RUT chileno
-  $.validator.addMethod(
-    "rutChileno",
-    function (value, element) {
-      // Eliminar puntos y guión del RUT
-      value = value.replace(/[.-]/g, "");
-      // Validar que el RUT tenga 8 o 9 dígitos
-      if (value.length < 8 || value.length > 9) {
-        return false;
-      }
-
-      // Validar que el último dígito sea un número o una 'K'
-      var validChars = "0123456789K";
-      var lastChar = value.charAt(value.length - 1).toUpperCase();
-      if (validChars.indexOf(lastChar) == -1) {
-        return false;
-      }
-
-      // Calcular el dígito verificador
-      var rut = parseInt(value.slice(0, -1), 10);
-      var factor = 2;
-      var sum = 0;
-      var digit;
-      while (rut > 0) {
-        digit = rut % 10;
-        sum += digit * factor;
-        rut = Math.floor(rut / 10);
-        factor = factor === 7 ? 2 : factor + 1;
-      }
-      var dv = 11 - (sum % 11);
-      dv = dv === 11 ? "0" : dv === 10 ? "K" : dv.toString();
-
-      // Validar que el dígito verificador sea correcto
-      return dv === lastChar;
-    },
-    "Por favor ingrese un RUT válido."
-  );
-
-  $("#register-form").validate({
+  $("#prod-form").validate({
     rules: {
       ID: {
         required: true,
-        ID: true,
+      },
+      nombre: {
+        required: true,
+      },
+      descripcion: {
+        required: true,
+      },
+      precio: {
+        required: true,
+      },
+      descuento_suscriptor: {
+        required: false,
+      },
+      descuento_oferta: {
+        required: false,
+      },
+    },
+    messages: {
+      ID: {
+        required: "Por favor ingrese un ID",
+      },
+      nombre: {
+        required: "Por favor ingrese el nombre del producto",
+      },
+      descripcion: {
+        required: "Por favor ingrese la descripción del producto",
+      },
+      precio: {
+        required: "Por favor ingrese el precio del producto",
+      },
+      descuento_suscriptor: {
+        required: "Por favor ingrese el precio del producto",
+      },
+      descuento_oferta: {
+        required: "Por favor ingrese el precio del producto",
+      },
+    },
+    errorElement: "div",
+    errorPlacement: function (error, element) {
+      error.addClass("invalid-feedback");
+      element.closest(".form-group").append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element)
+        .addClass(errorClass)
+        .removeClass(validClass)
+        .closest(".form-group")
+        .addClass("has-error");
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element)
+        .removeClass(errorClass)
+        .addClass(validClass)
+        .closest(".form-group")
+        .removeClass("has-error");
+    },
+  });
+
+  //Botón Guardar
+  $("#id_btnGuardar").on("click", function () {
+    if ($("#prod-form").valid()) {
+      // El formulario es válido, se puede enviar
+      $("#prod-form").submit();
+    }
+  });
+
+  //Boton Nuevo
+  $("#id_btnNuevo").click(function() {
+    $("#prod-form").reset();
+  });
+
+  //Boton Eliminar
+  $("#id_btnEliminar").click(function() {
+    $("#prod-form").reset();
+  });
+
+});
+
+//BODEGA//
+$(document).ready(function () {
+  $("#bodega-form").validate({
+    rules: {
+      cantidad: {
+        required: true,
+      },
+    },
+    messages: {
+      cantidad: {
+        required: "Por favor ingrese una cantidad",
+      },
+    },
+    errorElement: "div",
+    errorPlacement: function (error, element) {
+      error.addClass("invalid-feedback");
+      element.closest(".form-group").append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element)
+        .addClass(errorClass)
+        .removeClass(validClass)
+        .closest(".form-group")
+        .addClass("has-error");
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element)
+        .removeClass(errorClass)
+        .addClass(validClass)
+        .closest(".form-group")
+        .removeClass("has-error");
+    },
+  });
+
+  //Botón Agregar
+  $("#id_btnAgregar").on("click", function () {
+    if ($("#bodega-form").valid()) {
+      // El formulario es válido, se puede enviar
+      $("#bodega-form").submit();
+    }
+  });
+
+  //Boton Nuevo
+  $("#id_btnNuevo").click(function() {
+    $("#bodega-form").reset();
+  });
+});
+
+//METODO DE RUT CHILENO//
+$.validator.addMethod(
+  "rutChileno",
+  function (value, element) {
+    // Eliminar puntos y guión del RUT
+    value = value.replace(/[.-]/g, "");
+    // Validar que el RUT tenga 8 o 9 dígitos
+    if (value.length < 8 || value.length > 9) {
+      return false;
+    }
+
+    // Validar que el último dígito sea un número o una 'K'
+    var validChars = "0123456789K";
+    var lastChar = value.charAt(value.length - 1).toUpperCase();
+    if (validChars.indexOf(lastChar) == -1) {
+      return false;
+    }
+
+    // Calcular el dígito verificador
+    var rut = parseInt(value.slice(0, -1), 10);
+    var factor = 2;
+    var sum = 0;
+    var digit;
+    while (rut > 0) {
+      digit = rut % 10;
+      sum += digit * factor;
+      rut = Math.floor(rut / 10);
+      factor = factor === 7 ? 2 : factor + 1;
+    }
+    var dv = 11 - (sum % 11);
+    dv = dv === 11 ? "0" : dv === 10 ? "K" : dv.toString();
+
+    // Validar que el dígito verificador sea correcto
+    return dv === lastChar;
+  },
+  "Por favor ingrese un RUT válido."
+);
+
+//REGISTRO//
+$(document).ready(function () {
+  $("#sign-up-form").validate({
+    rules: {
+      rut: {
+        required: true,
+        rutChileno: true,
+      },
+      nombre: {
+        required: true,
+      },
+      apellido: {
+        required: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      direccion: {
+        required: true,
+      },
+      password: {
+        required: true,
+        minlength: 5,
+      },
+      password2: {
+        required: true,
+        equalTo: "#password",
+      },
+    },
+    messages: {
+      rut: {
+        required: "Por favor ingrese su RUT",
+        rutChileno: "Por favor ingrese un RUT válido",
+      },
+      nombre: {
+        required: "Por favor ingrese su nombre",
+      },
+      apellido: {
+        required: "Por favor ingrese su apellido",
+      },
+      email: {
+        required: "Por favor ingrese su correo electrónico",
+        email: "Por favor ingrese un correo electrónico válido",
+      },
+      direccion: {
+        required: "Por favor ingrese su dirección",
+      },
+      password: {
+        required: "Por favor ingrese su contraseña",
+        minlength: "La contraseña debe tener al menos 5 caracteres",
+      },
+      password2: {
+        required: "Por favor repita su contraseña",
+        equalTo: "Las contraseñas no coinciden",
+      },
+    },
+    errorElement: "div",
+    errorPlacement: function (error, element) {
+      error.addClass("invalid-feedback");
+      element.closest(".form-group").append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element)
+        .addClass(errorClass)
+        .removeClass(validClass)
+        .closest(".form-group")
+        .addClass("has-error");
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element)
+        .removeClass(errorClass)
+        .addClass(validClass)
+        .closest(".form-group")
+        .removeClass("has-error");
+    },
+  });
+
+  // Validación de checkbox opcional
+  $("#subscribe-check").on("change", function () {
+    $("#sign-up-form").validate().element("#subscribe-check");
+  });
+
+  //Botón Guardar
+  $("#id_btnGuardar").on("click", function () {
+    if ($("#sign-up-form").valid()) {
+      // El formulario es válido, se puede enviar
+      $("#sign-up-form").submit();
+    }
+  });
+
+  //Boton Limpiar
+  $("#id_btnLimpiar").click(function() {
+    $("#sign-up-form").reset();
+  });
+});
+
+//MIS DATOS//
+$(document).ready(function () {
+  $("#mydata-form").validate({
+    rules: {
+      rut: {
+        required: true,
+        rutChileno: true,
+      },
+      nombre: {
+        required: true,
+      },
+      apellido: {
+        required: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      direccion: {
+        required: true,
+      },
+      password: {
+        required: true,
+        minlength: 5,
+      },
+      password2: {
+        required: true,
+        equalTo: "#password",
+      },
+    },
+    messages: {
+      rut: {
+        required: "Por favor ingrese su RUT",
+        rutChileno: "Por favor ingrese un RUT válido",
+      },
+      nombre: {
+        required: "Por favor ingrese su nombre",
+      },
+      apellido: {
+        required: "Por favor ingrese su apellido",
+      },
+      email: {
+        required: "Por favor ingrese su correo electrónico",
+        email: "Por favor ingrese un correo electrónico válido",
+      },
+      direccion: {
+        required: "Por favor ingrese su dirección",
+      },
+      password: {
+        required: "Por favor ingrese su contraseña",
+        minlength: "La contraseña debe tener al menos 5 caracteres",
+      },
+      password2: {
+        required: "Por favor repita su contraseña",
+        equalTo: "Las contraseñas no coinciden",
+      },
+    },
+    errorElement: "div",
+    errorPlacement: function (error, element) {
+      error.addClass("invalid-feedback");
+      element.closest(".form-group").append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element)
+        .addClass(errorClass)
+        .removeClass(validClass)
+        .closest(".form-group")
+        .addClass("has-error");
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element)
+        .removeClass(errorClass)
+        .addClass(validClass)
+        .closest(".form-group")
+        .removeClass("has-error");
+    },
+  });
+
+  // Validación de checkbox opcional
+  $("#subscribe-check").on("change", function () {
+    $("#mydata-form").validate().element("#subscribe-check");
+  });
+
+  //Botón Guardar
+  $("#id_btnGuardar").on("click", function () {
+    if ($("#mydata-form").valid()) {
+      // El formulario es válido, se puede enviar
+      $("#mydata-form").submit();
+    }
+  });
+
+  //Boton Limpiar
+  $("#id_btnLimpiar").click(function() {
+    $("#mydata-form").reset();
+  });
+});
+
+//MANTENEDOR DE USUARIOS//
+$(document).ready(function () {
+  $("#user-form").validate({
+    rules: {
+
+      ID: {
+        required: true,
+      },
+      useropt: {
+        required: true,
+      },
+      nombre: {
+        required: true,
+      },
+      apellido: {
+        required: true,
       },
       rut: {
         required: true,
@@ -227,7 +436,6 @@ $(document).ready(function () {
       },
       direccion: {
         required: true,
-        direccion: true,
       },
       password: {
         required: true,
@@ -237,32 +445,135 @@ $(document).ready(function () {
         required: true,
         equalTo: "#password",
       },
-    }, // --> Fin de reglas
+    },
     messages: {
+
       ID: {
-        required: "El ID es un campo obligatorio",
-        ID: "El formato del ID no es válido",
+        required: "Por favor ingrese el ID",
+      },
+      useropt: {
+        required: "Por favor seleccione una opción",
+      },
+      nombre: {
+        required: "Por favor ingrese su nombre",
+      },
+      apellido: {
+        required: "Por favor ingrese su apellido",
       },
       rut: {
-        required: "El rut es un campo obligatorio",
-        rutChileno: "El formato del rut no es válido",
+        required: "Por favor ingrese su RUT",
+        rutChileno: "Por favor ingrese un RUT válido",
       },
       email: {
-        required: "El email es un campo requerido",
-        email: "El email no cumple el formato de un correo",
+        required: "Por favor ingrese su correo electrónico",
+        email: "Por favor ingrese un correo electrónico válido",
       },
       direccion: {
-        required: "La direccion es un campo requerido",
-        direccion: "La direccion no cumple el formato correcto",
+        required: "Por favor ingrese su dirección",
       },
       password: {
-        required: "La contraseña es una campo obligatorio",
-        minlength: "Mínimo 5 caracteres",
+        required: "Por favor ingrese su contraseña",
+        minlength: "La contraseña debe tener al menos 5 caracteres",
       },
       password2: {
-        required: "Repita la contraseña anterior",
-        equalTo: "Debe ser igual al campo contraseña",
+        required: "Por favor repita su contraseña",
+        equalTo: "Las contraseñas no coinciden",
       },
     },
+    errorElement: "div",
+    errorPlacement: function (error, element) {
+      error.addClass("invalid-feedback");
+      element.closest(".form-group").append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element)
+        .addClass(errorClass)
+        .removeClass(validClass)
+        .closest(".form-group")
+        .addClass("has-error");
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element)
+        .removeClass(errorClass)
+        .addClass(validClass)
+        .closest(".form-group")
+        .removeClass("has-error");
+    },
+  });
+
+  // Validación de checkbox opcional
+  $("#subscribe-check").on("change", function () {
+    $("#user-form").validate().element("#subscribe-check");
+  });
+
+  //Botón Guardar
+  $("#id_btnGuardar").on("click", function () {
+    if ($("#user-form").valid()) {
+      // El formulario es válido, se puede enviar
+      $("#user-form").submit();
+    }
+  });
+
+  //Boton Nuevo
+  $("#id_btnNuevo").click(function() {
+    $("#user-form").reset();
+  });
+
+  //Boton Eliminar
+  $("#id_btnEliminar").click(function() {
+    $("#user-form").reset();
+  });
+});
+
+//LOGIN//
+$(document).ready(function () {
+  $("#login-form").validate({
+    rules: {
+      email: {
+        required: true,
+        email: true,
+      },
+      password: {
+        required: true,
+        minlength: 5,
+      },
+    },
+    messages: {
+      email: {
+        required: "Por favor ingrese su correo electrónico",
+        email: "Por favor ingrese un correo electrónico válido",
+      },
+      password: {
+        required: "Por favor ingrese su contraseña",
+        minlength: "La contraseña debe tener al menos 5 caracteres",
+      },
+    },
+    errorElement: "div",
+    errorPlacement: function (error, element) {
+      error.addClass("invalid-feedback");
+      element.closest(".form-group").append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element)
+        .addClass(errorClass)
+        .removeClass(validClass)
+        .closest(".form-group")
+        .addClass("has-error");
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element)
+        .removeClass(errorClass)
+        .addClass(validClass)
+        .closest(".form-group")
+        .removeClass("has-error");
+    },
+  });
+
+  //Botón Iniciar Sesion
+  $("#id_btnIniciarSesion").on("click", function () {
+    if ($("#login-form").valid()) {
+      // El formulario es válido, se puede enviar
+      $("#login-form").submit();
+    }
   });
 });
